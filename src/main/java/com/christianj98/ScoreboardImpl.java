b32f3d967e1397f84efbda2e5f88b6a1dd9b2c73
Kania <kania.praca@wp.pl>
2025-03-22 15:07:15 +0100

    Handle invalid team names

--- src/main/java/com/christianj98/ScoreboardImpl.java
+++ src/main/java/com/christianj98/ScoreboardImpl.java
@@ -1,11 +1,17 @@
 package com.christianj98;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.UUID;
 
 public class ScoreboardImpl implements Scoreboard {
 
     @Override
     public UUID startFootballMatch(String homeTeam, String awayTeam) {
+        if (StringUtils.isBlank(homeTeam) || StringUtils.isBlank(awayTeam)) {
+            throw new IllegalArgumentException("Team names cannot be null or blank!");
+        }
+
         return null;
     }
 
